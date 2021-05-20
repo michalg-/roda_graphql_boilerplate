b7738723277d985c740ce1a440089ab766e113da
Michał Gilewski <michal.gilewski@start-up.house>
2021-05-20 09:34:58 +0200

    Add logger to graphql action

--- routes.rb
+++ routes.rb
@@ -4,12 +4,22 @@ class Application < Roda
   route do |r|
     r.on('graphql') do
       r.post do
+        context = {}
+
         result = Schema.execute(
           request.params['query'],
-          variables: request.params['variables']
+          variables: request.params['variables'],
+          context: context,
+          operation_name: request.params['operationName']
         )
 
         result.to_json
+      rescue => e
+        raise e unless Env.development?
+        $stderr.print(e.message)
+        $stderr.print(e.backtrace.join("\n"))
+        response.status = 500
+        { errors: [{ message: e.message, backtrace: e.backtrace }], data: {} }
       end
     end
   end
